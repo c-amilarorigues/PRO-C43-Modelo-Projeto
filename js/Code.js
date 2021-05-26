551565edeb90651faa5da7738855aeac88ed8b9f
c-amilarorigues <81708586+c-amilarorigues@users.noreply.github.com>
2021-05-26 20:56:57 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,15 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+// adicione código para exibir o restante dos nomes e as dicas.
+    
+
+}
